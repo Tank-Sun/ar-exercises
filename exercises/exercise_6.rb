097e01a208e6c1d35013f7002453729cfa37136f
Yongjia Sun <thomastank0926@gmail.com>
2022-11-16 10:35:55 +0000

    associate employees table with stores table, create employees data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "James", last_name: "Harden", hourly_rate: 100)
+@store1.employees.create(first_name: "Tray", last_name: "Young", hourly_rate: 80)
+
+@store2.employees.create(first_name: "San", last_name: "Zhang", hourly_rate: 60)
+@store2.employees.create(first_name: "Si", last_name: "Li", hourly_rate: 70)
+@store2.employees.create(first_name: "Meimei", last_name: "Han", hourly_rate: 80)
+@store2.employees.create(first_name: "Lei", last_name: "Li", hourly_rate: 90)
